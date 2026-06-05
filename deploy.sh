ef8e326c8b2d24e7b3a1163d3adbb55d987a5ec7
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2026-06-05 17:34:55 -0300

    Add compilation to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+npm run build || exit 1
+
 cp main.js '/Users/taghawi/Library/Mobile Documents/iCloud~md~obsidian/Documents/Davoud Taghawinejad Zettelkasten/.obsidian/plugins/kanban-board'
 cp manifest.json '/Users/taghawi/Library/Mobile Documents/iCloud~md~obsidian/Documents/Davoud Taghawinejad Zettelkasten/.obsidian/plugins/kanban-board'
 
